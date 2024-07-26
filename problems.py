8c8b138170158d3d73a40530558cfafa57b54f4b
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2024-07-26 15:07:32 +0800

    Add files via upload

--- problems.py
+++ problems.py
@@ -1325,3 +1325,39 @@ else:
             minCG -= 1
         print('1:',result)
         print('2:',factorial(maxAU)/factorial(maxAU-minAU))
+        
+#Partial Gene Orderings
+'''
+A partial permutation is an ordering of only k
+ objects taken from a collection containing n
+ objects (i.e., k≤n
+). For example, one partial permutation of three of the first eight positive integers is given by (5,7,2)
+.
+
+The statistic P(n,k)
+ counts the total number of partial permutations of k
+ objects that can be formed from a collection of n
+ objects. Note that P(n,n)
+ is just the number of permutations of n
+ objects, which we found to be equal to n!=n(n−1)(n−2)⋯(3)(2)
+ in “Enumerating Gene Orders”.
+ 
+ Given: Positive integers n and k
+ such that 100≥n>0 and 10≥k>0.
+
+Return: The total number of partial permutations P(n,k)
+, modulo 1,000,000.
+'''
+N = 92 
+k = 8
+def factorial(N):
+    if N == 0 or N == 1:
+        return 1
+    elif N >1:
+        return N*factorial(N-1)
+
+P = factorial(N)/factorial(N-k)
+print(np.mod(P,1000000))
+
+
+
